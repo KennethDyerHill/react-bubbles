86bf831d706d88aa75cc19505cab160c4faa0130
KennethDyerHill <52476371+KennethDyerHill@users.noreply.github.com>
2020-04-08 20:39:14 -0400

    Update

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { axiosWithAuth } from "./axios/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -21,10 +21,34 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+      .put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorEdit)
+      .then(res => {
+        console.log(res.data);
+        updateColors([...colors, res.data]);
+        console.log(colors);
+        setEditing(false);
+        fetchBubbles();
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    console.log(color);
+    axiosWithAuth()
+      .delete(`http://localhost:5000/api/colors/${color.id}`)
+      .then(res => {
+        console.log(res.data);
+        console.log("deleted color!");
+        updateColors(colors.filter(item => item.id !== color.id));
+      })
+      .catch(err => {
+        console.log(err);
+        alert("An error occured!");
+      });
   };
 
   return (
